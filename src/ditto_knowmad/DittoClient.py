24aa24f8706c06c5913b53b39e96f53a35a1e79d
computabeast <seun@aya.yale.edu>
2023-05-23 10:29:18 -0400

    allow client to use date ranges

--- src/ditto_knowmad/DittoClient.py
+++ src/ditto_knowmad/DittoClient.py
@@ -1,7 +1,8 @@
 import asyncio
 from enum import Enum
+import os 
 
-from .backend_requests import make_request, make_sync_request, make_handle_request, make_sync_handle_request
+from .backend_requests import make_request, make_sync_request
 
 class ApiKeyException(Exception):
     pass
@@ -10,26 +11,36 @@ class DittoClient():
     def __init__(self, api_key):
         self.api_key = api_key
 
-    def get_posts_from_source(self, query: str, sources: list, sync=False) -> object:
+    def get_posts_from_source(
+            self,
+            query: str, 
+            sources: list, 
+            mode: str = 'posts',
+            sync: bool = False,
+            dates: list = None
+            ) -> object:
+        
         if not self.api_key:
             raise ApiKeyException("No API Key was set when instantiating DittoClient object.")
         
         if sync:
             print("Waiting on API...")
-            response = make_sync_request(query=query, sources=sources, api_key=self.api_key)
+            response = make_sync_request(
+                query=query, 
+                sources=sources, 
+                api_key=self.api_key,
+                mode=mode,
+                dates=dates
+                )
         else:
-            response = asyncio.run(make_request(query=query, sources=sources, api_key=self.api_key))
-
-        return response
-
-    def get_tweets_by_handle(self, query: str, sync=False) -> object:
-        if not self.api_key:
-                raise ApiKeyException("No API Key was set when instantiating DittoClient object.")
-        
-        if sync:
-            print("Waiting on API...")
-            response = make_sync_handle_request(query=query, api_key=self.api_key)
-        else:
-            response = asyncio.run(make_handle_request(query=query, api_key=self.api_key))
+            response = asyncio.run(
+                make_request(
+                    query=query, 
+                    sources=sources, 
+                    api_key=self.api_key,
+                    mode=mode,
+                    dates=dates
+                    )
+                )
 
         return response
--- src/ditto_knowmad/backend_requests.py
+++ src/ditto_knowmad/backend_requests.py
@@ -7,11 +7,16 @@ class BackendResponseException(Exception):
 class ClientRequestException(Exception):
     pass
 
-async def make_request(query: str, api_key: str, sources: list):
+async def make_request(query: str, api_key: str, sources: list, mode: str, dates: tuple = None):
+    url = f'https://api.ditto.fyi/get/raw_query?q={query}&s={",".join(sources)}&m={mode}'
+
+    if dates[0] and dates[1]:
+        url += f'&sd={dates[0]}&ed={dates[1]}'
+
     try:
       headers = {'X-API-Key': api_key}
       async with aiohttp.ClientSession() as session:
-          async with session.get(f'https://api.ditto.fyi/get/raw_query?q={query}&s={",".join(sources)}', headers=headers) as response:
+          async with session.get(url, headers=headers) as response:
               if response.status == 200:
                   data = await response.json()
                   return data
@@ -24,43 +29,15 @@ async def make_request(query: str, api_key: str, sources: list):
     except aiohttp.ClientError as e:
         raise ClientRequestException(f"Request failed {e}")
 
-def make_sync_request(query: str, api_key: str, sources: list): 
-    try:
-        headers = {'X-API-Key': api_key}
-        response = requests.get(f'https://api.ditto.fyi/get/raw_query?q={query}&s={",".join(sources)}', headers=headers)
-        if response.status_code == 200:
-            data = response.json()
-            return data
-        elif response.status_code == 400:
-            raise BackendResponseException("Bad Request: Check your request parameters.")
-        elif response.status_code == 401:
-            raise BackendResponseException("Unauthorized: Invalid Credentials, check your API key")
-        else:
-            raise BackendResponseException(f"Server error: {response.status_code}")
-    except requests.exceptions.RequestException as e:
-        raise ClientRequestException(f"Request failed {e}")
+def make_sync_request(query: str, api_key: str, sources: list, mode: str, dates: tuple = None): 
+    url = f'https://api.ditto.fyi/get/raw_query?q={query}&s={",".join(sources)}&m={mode}'
 
-async def make_handle_request(query: str, api_key: str):
-    try:
-      headers = {'X-API-Key': api_key}
-      async with aiohttp.ClientSession() as session:
-          async with session.get(f'https://api.ditto.fyi/get/by_twitter_handle?q={query}', headers=headers) as response:
-              if response.status == 200:
-                  data = await response.json()
-                  return data
-              elif response.status == 400:
-                  raise BackendResponseException("Bad Request: Check your request parameters.")
-              elif response.status == 401:
-                  raise BackendResponseException("Unauthorized: Invalid Credentials, check your API key")
-              else:
-                  raise BackendResponseException(f"Server error: {response.status}")
-    except aiohttp.ClientError as e:
-        raise ClientRequestException(f"Request failed {e}")
-            
-def make_sync_handle_request(query: str, api_key: str):
+    if dates and dates[0] and dates[1]:
+        url += f'&sd={dates[0]}&ed={dates[1]}'
+
     try:
         headers = {'X-API-Key': api_key}
-        response = requests.get(f'https://api.ditto.fyi/get/by_twitter_handle?q={query}', headers=headers)
+        response = requests.get(url, headers=headers)
         if response.status_code == 200:
             data = response.json()
             return data
